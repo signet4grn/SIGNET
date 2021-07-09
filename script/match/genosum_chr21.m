9d5f88d8dcc2ad8f0fea527536f94abdbcdcc282
jiang548 <jiang548@purdue.edu>
2021-07-09 01:24:14 -0400

    deleting genosum

--- script/match/genosum_chr21.m
+++
@@ -1,6 +0,0 @@
-%%% genosum.m
-%%% Calculate the minor allele counts for a chromosome
-%
-data=load('matched.Geno_chr21.data');
-ma=sum(data,1);   %? ma=sum(data,2);
-dlmwrite('matched.Geno.ma_chr21',ma,' ');
--- script/match/genosum_chr6.m
+++
@@ -1,6 +0,0 @@
-%%% genosum.m
-%%% Calculate the minor allele counts for a chromosome
-%
-data=load('matched.Geno_chr6.data');
-ma=sum(data,1);   %? ma=sum(data,2);
-dlmwrite('matched.Geno.ma_chr6',ma,' ');
