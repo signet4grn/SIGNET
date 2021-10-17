99dfbac824455cad0ff94f66b7afdeeb080e79d8
Zhongli Jiang <jiang548@morgan.stat.purdue.edu>
2021-10-17 00:27:07 -0400

    Geno.eval

--- script/match/match_pca.sh
+++ script/match/match_pca.sh
@@ -1,5 +1,5 @@
 cd $SIGNET_TMP_ROOT/tmpm
 
 Rscript $SIGNET_SCRIPT_ROOT/match/pca_prepare.R &&
-smartpca.perl -i new.Geno.eigenstratgeno -a new.Geno.snp -b new.Geno.ind -k 15 -o Geno.pca -e Geno.eval -l Geno.log -q NO -m 1 -p Geno.plot > /dev/null &&
+smartpca.perl -i new.Geno.eigenstratgeno -a new.Geno.snp -b new.Geno.ind -k 15 -o Geno.pca -e "Geno.eval" -l Geno.log -q NO -m 1 -p Geno.plot > /dev/null &&
 Rscript $SIGNET_SCRIPT_ROOT/match/pca_vis.R
