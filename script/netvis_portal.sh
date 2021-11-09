15995fe788d1a8dce661eeb03c871365a479c9db
jiang548 <jiang548@purdue.edu>
2021-11-08 20:00:43 -0500

    visualization file manage

--- script/netvis_portal.sh
+++ script/netvis_portal.sh
@@ -3,8 +3,10 @@
 cmdprefix="$SIGNET_ROOT/signet -s --"
 freq=$(${cmdprefix}freq);
 ntop=$(${cmdprefix}ntop)
+tmpv=$(${cmdprefix}tmpv)
+resv=$(${cmdprefix}resv)
 
-ARGS=`getopt -a -o r:,h -l ntop:,n:,freq:,f:,h:,help -- "$@"`
+ARGS=`getopt -a -o r:,h -l ntop:,n:,freq:,f:,tmpv:,resv:,h:,help -- "$@"`
 
 function usage() {
 	echo 'Usage:'
@@ -13,8 +15,10 @@ function usage() {
 	echo 'Description:'
         echo '  --freq FREQENCY              bootstrap frequecy for the visualization'
 	echo '  --ntop N_TOP                 number of sub-networks'
+        echo '  --tmpv                       set the temporary file directory'
+        echo '  --resv                       set the result file directory'
 	echo '  --help                       usage'
-	exit
+	exit -1
 }
 
 [ $? -ne 0 ] && usage
@@ -34,6 +38,14 @@ case "$1" in
                 ${cmdprefix}ntop $ntop
 		shift
 		;;
+	--tmpv)
+                tmpv=$2
+                $SIGNET_ROOT/signet -s --tmpv $tmpv
+                shift;;
+        --resv)
+                resv=$2
+                $SIGNET_ROOT/signet -s --resv $resv
+                shift;;
 	--h | --help)
 		usage
 		exit
@@ -46,8 +58,24 @@ case "$1" in
 shift
 done 
 
+file_compare $tmpv $resv
+
+## Do a file check
+file_check $tmpv
+file_check $resv
+
 mkdir -p $SIGNET_TMP_ROOT/tmpv
 mkdir -p $SIGNET_RESULT_ROOT/resv
 mkdir -p $SIGNET_DATA_ROOT/netvis
 
-$SIGNET_SCRIPT_ROOT/netvis/netvis.sh $freq $ntop
+$SIGNET_SCRIPT_ROOT/netvis/netvis.sh $freq $ntop  
+
+cd $SIGNET_TMP_ROOT/tmpv
+file_prefix signet
+cd $cwd
+file_trans $SIGNET_TMP_ROOT/tmpv/signet $tmpv
+
+cd $SIGNET_RESULT_ROOT/resv
+file_prefix signet
+cd $cwd
+file_trans $SIGNET_RESULT_ROOT/resv/signet $resv
