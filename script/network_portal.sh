ebf2eb5b40de5f014235f7691d4aa39723937ce9
jiang548 <jiang548@purdue.edu>
2021-11-08 19:55:06 -0500

    network file manage

--- script/network_portal.sh
+++ script/network_portal.sh
@@ -8,8 +8,11 @@ ncores=$(${cmdprefix}ncores);
 queue=$(${cmdprefix}queue);
 memory=$(${cmdprefix}memory);
 walltime=$(${cmdprefix}walltime)
+cwd=$(pwd)
+tmpn=$($SIGNET_ROOT/signet -s --tmpn)
+resn=$($SIGNET_ROOT/signet -s --resn)
 
-ARGS=`getopt -a -o r: -l loc:,l:,ncis:,r:,cor:,memory:,m:,queue:,q:,walltime:,:w:,nboots:,h:,help -- "$@"`
+ARGS=`getopt -a -o r: -l loc:,l:,ncis:,r:,cor:,memory:,m:,queue:,q:,walltime:,:w:,nboots:,tmpn:,resn:,h:,help -- "$@"`
 
 function usage() {
 	echo 'Usage:'
@@ -23,7 +26,9 @@ function usage() {
 	echo '  --queue QUEUE                 queue name'
         echo '  --walltime WALLTIME		maximum walltime of the server in seconds'
 	echo '  --nboots NBOOTS               number of bootstraps datasets'                   
-	exit
+        echo "  --tmpn                        set the temporary file directory"
+        echo "  --resn                        set the result file directory"
+	exit -1 
 }
 [ $? -ne 0 ] && usage
 
@@ -62,6 +67,14 @@ case "$1" in
 		walltime=$2
 		${cmdprefix}walltime $walltime
                 shift;;
+	--tmpn)
+                tmpn=$2
+                $SIGNET_ROOT/signet -s --tmpn $tmpn
+                shift;;
+        --resn)
+                resn=$2
+                $SIGNET_ROOT/signet -s --resn $resn
+                shift;;
 	--h|--help)
 		usage
 		exit
@@ -74,8 +87,24 @@ case "$1" in
 shift
 done 
 
+file_compare $tmpn $resn
+
+## Do a file check
+file_check $tmpn
+file_check $resn
+
 mkdir -p $SIGNET_TMP_ROOT/tmpn
 mkdir -p $SIGNET_RESULT_ROOT/resn
 mkdir -p $SIGNET_DATA_ROOT/network
 
 $SIGNET_SCRIPT_ROOT/network/network.sh $nboots $cor $queue $ncores $memory $walltime $loc
+
+cd $SIGNET_TMP_ROOT/tmpn
+file_prefix signet
+cd $cwd
+file_trans $SIGNET_TMP_ROOT/tmpn/signet $tmpn
+
+cd $SIGNET_RESULT_ROOT/resn
+file_prefix signet
+cd $cwd
+file_trans $SIGNET_RESULT_ROOT/resn/signet $resn
