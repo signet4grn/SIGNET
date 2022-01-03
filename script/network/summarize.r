9115ba20a47909f084aa2804b2836cb65c1a1460
jiang548 <jiang548@purdue.edu>
2022-01-03 15:51:48 -0500

    bootstrap frequency summary

--- script/network/summarize.r
+++ script/network/summarize.r
@@ -16,3 +16,12 @@ for (i in 1:N) {
 }
 fwrite(Afreq, paste0(Sys.getenv("resn"), "_Afreq"), row.names=F,col.names=F)
 
+freq <- c(0.8, 0.85, 0.9, 0.95, 0.98, 0.99, 1)
+for(freqi in freq){
+  freq_count <- matrix(0, nrow(Afreq), ncol(Afreq))
+  freq_count[Afreq >= freqi ] <- 1
+  edge_count <- sum(freq_count)
+  node <- which(freq_count!=0, arr.ind=T)
+  node_count <- length(unique((c(node[, 1], node[, 2]))))  
+  cat(paste0("There are ", node_count, " genes and  ", edge_count, " connections in the network with bootstrap frequency at least ", freqi, "\n")) 
+}
