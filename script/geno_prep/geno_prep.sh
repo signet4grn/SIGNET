32c5c21e291ef2f6d3ab298d0b4690f243714243
Zhongli Jiang <jiang548@morgan.stat.purdue.edu>
2022-01-29 19:30:35 -0500

    typo

--- script/geno_prep/geno_prep.sh
+++ script/geno_prep/geno_prep.sh
@@ -1,3 +1,3 @@
-#$SIGNET_SCRIPT_ROOT/geno_prep/plink.sh  && 
-#$SIGNET_SCRIPT_ROOT/geno_prep/imputation.sh  
+$SIGNET_SCRIPT_ROOT/geno_prep/plink.sh  && 
+$SIGNET_SCRIPT_ROOT/geno_prep/imputation.sh  
 $SIGNET_SCRIPT_ROOT/geno_prep/combine_geno.sh 
