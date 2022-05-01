92fc3e28f294ff74c75840e7f8cccd96c16310ec
Zhongli Jiang <jiang548@morgan.stat.purdue.edu>
2022-05-01 18:45:00 -0400

    add wait command

--- script/cis-eQTL/cis-SNPs.sh
+++ script/cis-eQTL/cis-SNPs.sh
@@ -11,11 +11,12 @@ awk -f low.snps.idx < $geno 1>low.Geno.data 2>err_low &
 awk -f common.snps.idx < $geno 1>common.Geno.data 2>err_common &
 
 ### SNP position (chr#, SNP pos)
-awk '{print $1,$4}' $snps_map > all.SNPpos &
-awk '{print $1,$4}' rare.Geno.map > rare.SNPpos &
-awk '{print $1,$4}' low.Geno.map > low.SNPpos &
-awk '{print $1,$4}' common.Geno.map > common.SNPpos &
+awk '{print $1,$4}' $snps_map > all.SNPpos
+awk '{print $1,$4}' rare.Geno.map > rare.SNPpos 
+awk '{print $1,$4}' low.Geno.map > low.SNPpos 
+awk '{print $1,$4}' common.Geno.map > common.SNPpos 
 
 Rscript $SIGNET_SCRIPT_ROOT/cis-eQTL/rare.cispair.r "upstream='$upstream'" "downstream='$downstream'" &
 Rscript $SIGNET_SCRIPT_ROOT/cis-eQTL/common.cispair.r "upstream='$upstream'" "downstream='$downstream'" &
 Rscript $SIGNET_SCRIPT_ROOT/cis-eQTL/low.cispair.r "upstream='$upstream'" "downstream='$downstream'" &
+wait
