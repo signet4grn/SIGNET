dbf819bff5641cb5611d371faf207df18c84de14
Zhang, Dabao <zhangdb@purdue.edu>
2022-05-20 17:00:29 -0400

    Update plink.sh

--- script/geno_prep/plink.sh
+++ script/geno_prep/plink.sh
@@ -6,4 +6,4 @@ plink --silent --noweb --ped $pedfile --map $mapfile --geno $geno --hwe $hwe --r
 plink --silent --noweb --file $SIGNET_TMP_ROOT/tmpg/clean_Genotype --mind $mind --exclude $SIGNET_TMP_ROOT/tmpg/clean_Genotype.dupvar --recode --out $SIGNET_TMP_ROOT/tmpg/clean_Genotype --set-hh-missing &&
 plink --silent --noweb --file $SIGNET_TMP_ROOT/tmpg/clean_Genotype --recodeA --out $SIGNET_TMP_ROOT/tmpg/clean_Genotype --set-hh-missing &&
 tail -n+2 $SIGNET_TMP_ROOT/tmpg/clean_Genotype.raw > $SIGNET_TMP_ROOT/tmpg/clean_Genotype.data  &&
-echo -e "Preprocessing genotype data... Completed!\n"
+echo -e "Preprocessing genotype data... completed!\n"
