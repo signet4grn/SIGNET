ad58cadfcaaef7cff986b270754191370792227d
Zhang, Dabao <zhangdb@purdue.edu>
2022-05-20 14:28:30 -0400

    Update extractgeno.pl

--- script/adj/extractgeno.pl
+++ script/adj/extractgeno.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # extractrgeno.pl file
 my @IDX;
-open(IDXLIST, $ARGV[0]) || die "Couldn't open $ARGV[0]\n";
+open(IDXLIST, $ARGV[0]) || die "Error: Cannot open $ARGV[0]\n";
 while(<IDXLIST>) {
 chomp;
 push(@IDX,$_);
@@ -9,7 +9,7 @@ push(@IDX,$_);
 close(IDXLIST);
 
 $linescount=1;
-open(MAP, $ARGV[1]) || die "Couldn't open $ARGV[1]\n";
+open(MAP, $ARGV[1]) || die "Error: Cannot open $ARGV[1]\n";
 while(<MAP>) {
 
 while ($linescount eq $IDX[0]) {
