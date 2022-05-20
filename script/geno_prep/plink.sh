492cba9c5c1fa5700fd05825f0bcf3aa469823f5
Zhang, Dabao <zhangdb@purdue.edu>
2022-05-20 13:27:28 -0400

    Update plink.sh

--- script/geno_prep/plink.sh
+++ script/geno_prep/plink.sh
@@ -1,9 +1,9 @@
 ## This sciprt will apply plink commands to the files
 
-echo -e "Preprocessing using Plink ...\n"
+echo -e "Preprocessing genotype data with PLINK...\n"
 
 plink --silent --noweb --ped $pedfile --map $mapfile --geno $geno --hwe $hwe --recode --snps-only --list-duplicate-vars suppress-first --out $SIGNET_TMP_ROOT/tmpg/clean_Genotype &&
 plink --silent --noweb --file $SIGNET_TMP_ROOT/tmpg/clean_Genotype --mind $mind --exclude $SIGNET_TMP_ROOT/tmpg/clean_Genotype.dupvar --recode --out $SIGNET_TMP_ROOT/tmpg/clean_Genotype --set-hh-missing &&
 plink --silent --noweb --file $SIGNET_TMP_ROOT/tmpg/clean_Genotype --recodeA --out $SIGNET_TMP_ROOT/tmpg/clean_Genotype --set-hh-missing &&
 tail -n+2 $SIGNET_TMP_ROOT/tmpg/clean_Genotype.raw > $SIGNET_TMP_ROOT/tmpg/clean_Genotype.data  &&
-echo -e "Plink preprocessing finished\n"
+echo -e "Preprocessing genotype data... Completed!\n"
