33872720253ab2feb15790961e78fa1c1eb5d379
Zhang, Dabao <zhangdb@purdue.edu>
2022-05-20 14:56:12 -0400

    Update common.ciseQTL.sh

--- script/cis-eQTL/common.ciseQTL.sh
+++ script/cis-eQTL/common.ciseQTL.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd $SIGNET_TMP_ROOT/tmpc
 
-echo -e "Cis-eQTL Analyis for common variants [alpha:"$alpha"]......\n"
+echo -e "Mapping cis-eQTL with common variants [alpha:"$alpha"]...\n"
 
 Rscript $SIGNET_SCRIPT_ROOT/cis-eQTL/common.ciseQTL.r  
 
-echo -e "\nSummarizing the result for common variants \n"
+echo -e "\nSummarizing the result for common variants...\n"
 
 ##select significant ones 
 Rscript $SIGNET_SCRIPT_ROOT/cis-eQTL/sig.commoncis.r "alpha='$alpha'"
@@ -17,5 +17,4 @@ wait
 
 nsig=$(awk '{print $2}' ${resc}_common.sig.pValue_${alpha} | sort | uniq | wc -l)
 ngene=$(awk '{print $1}' ${resc}_common.sig.pValue_${alpha} | sort | uniq | wc -l)
-echo -e "----" $nsig "significant common variants found for "$ngene" genes\n" 
-
+echo -e "----" $nsig "significant common variants found for "$ngene" genes!\n" 
