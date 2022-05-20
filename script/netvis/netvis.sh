2701497874e9947fab7228cfc935953f94068ba5
Zhang, Dabao <zhangdb@purdue.edu>
2022-05-20 15:14:52 -0400

    Update netvis.sh

--- script/netvis/netvis.sh
+++ script/netvis/netvis.sh
@@ -2,12 +2,12 @@
 
 cd $SIGNET_RESULT_ROOT/resv
 
-echo -e "Extracting edges...\n"
+echo -e "Extracting directed regulations...\n"
 Rscript $SIGNET_SCRIPT_ROOT/netvis/extract_edges.R &&
 
-echo -e "Loading results in shiny app...\n"
+echo -e "Loading results into Shiny app...\n"
 Rscript $SIGNET_SCRIPT_ROOT/netvis/netshiny.R &&
 #Rscript -e 'shiny::runApp(paste0(Sys.getenv("SIGNET_SCRIPT_ROOT"), "/netvis/netshiny.R"), launch.browser = TRUE)' &&
 
 echo -e "Genes for top "$ntop" networks has been returned to the folder" &&
-echo -e "You can check the summary in shiny dashboard using web browser or the visualizing result end with .html in the resv foldern"
+echo -e "You can check the summary in Shiny dashboard with web browser or the visualizing results in .html files inside folder resv\n"
