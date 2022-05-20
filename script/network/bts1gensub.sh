bc8d8df7b7e659f859ed305b73bb245ab8cfab51
Zhang, Dabao <zhangdb@purdue.edu>
2022-05-20 15:35:41 -0400

    Update bts1gensub.sh

--- script/network/bts1gensub.sh
+++ script/network/bts1gensub.sh
@@ -91,9 +91,9 @@ echo -e "\n"
 grep "^sbatch" qsub1.sh > job1_command
 if [[ $interactive == "T" || $interactive == "True" || $interactive == "TRUE" ]];then
 sed -i 's/sbatch -W/srun -n1 bash/g;s/&/>\/dev\/null/g' qsub1.sh
-echo -e "Running $(wc -l < job1_command) jobs ... \n"
+echo -e "Running $(wc -l < job1_command) jobs...\n"
 else
-echo -e "Submitting $(wc -l < job1_command) jobs ... \n"
+echo -e "Submitting $(wc -l < job1_command) jobs...\n"
 fi
 
 time sh qsub1.sh | tee submit1_log 
