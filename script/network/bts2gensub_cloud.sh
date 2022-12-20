c6193f033ca1d6fdaf168f4826b260daa3d0c2ab
geomeday <jiang548@purdue.edu>
2022-12-19 21:45:59 -0500

    remove combiner

--- script/network/bts2gensub_cloud.sh
+++ script/network/bts2gensub_cloud.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo -e "Combining results in stage1...\n"
+#echo -e "Combining results in stage1...\n"
 # summarize the result in stage1
 
-mkdir -p $SIGNET_TMP_ROOT/tmpn/stage1/output
-for i in $( seq 0 $nboots )
-do
-  paste -d' ' $(find ./ -name '$SIGNET_TMP_ROOT/tmpn/stage1/*ypre'$i'_*' | sort -V) > $SIGNET_TMP_ROOT/tmpn/stage1/output/ypre$i
-done
+#mkdir -p $SIGNET_TMP_ROOT/tmpn/stage1/output
+#for i in $( seq 0 $nboots )
+#do
+#  paste -d' ' $(find ./ -name '$SIGNET_TMP_ROOT/tmpn/stage1/*ypre'$i'_*' | sort -V) > $SIGNET_TMP_ROOT/tmpn/stage1/output/ypre$i
+#done
 
-echo -e "Combining finished...\n"
+#echo -e "Combining finished...\n"
 
 echo -e "Testing on the first bootstrap data with the first 10 genes...\n"
 
