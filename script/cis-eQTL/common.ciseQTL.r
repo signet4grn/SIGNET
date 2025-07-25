2440a3515ccf25ed6c5395ded06c3cb3282cc754
catstats <jiang548@purdue.edu>
2025-07-25 15:04:51 -0700

    Update common.ciseQTL.r  to record the effective size

--- script/cis-eQTL/common.ciseQTL.r
+++ script/cis-eQTL/common.ciseQTL.r
@@ -12,15 +12,25 @@ idx=read.table('common.cispair.idx')
 len=dim(idx)[1]
 
 p=matrix(0,len,1)
+beta = matrix(0, len, 1)
+
 for (i in 1:len){
-  if(any(is.nan(x[,idx[i,2]])))
+  gexp = y[, idx[i, 1]]
+  geno = x[, idx[i, 2]]
+  
+  if(any(is.nan(geno)))
     p[i] <- 1
+    beta[i] <- NA
   else {
-    fit=lm(y[,idx[i,1]]~x[,idx[i,2]])
-    p[i]=summary(fit)$coefficients[2,4]
+    fit=lm(gexp ~ geno)
+    beta[i] = summary(fit)$coefficients[2, 1]
+    p[i]=summary(fit)$coefficients[2, 4]
   }
   if( (i%%10000)==0)  print(i)
 }
 
 p=cbind(idx,p) # Col 1 is index of gene, Col 2 is index of its cis-SNP, Col 3 is p-value
 write.table(p,"common.pValue",row.names=F,col.names=F,quote=F,sep=" ")
+
+res = cbind(idx, beta, p)
+write.table(res, "common.pValue.effect", row.names = F, col.names = F, quote = F, sep = " ")
